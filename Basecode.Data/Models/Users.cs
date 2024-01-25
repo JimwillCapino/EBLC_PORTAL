0d13ccb7edd5c201eb223c7da19025a2605cce4c
JimwillCapino <jimwillcapino@gmail.com>
2024-01-26 00:54:53 +0800

    User model created.

---
+++ Basecode.Data/Models/Users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Data.Models
+{
+    public  class Users
+    {
+        public int UID { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string email { get; set; }
+        public string sex {  get; set; }
+        public string role { get; set; }
+        public byte[]? ProfilePic { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
