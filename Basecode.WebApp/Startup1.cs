4f1e3c3e3db0b8ff6ea8ae4f02205e9cf3faf916
JimwillCapino <jimwillcapino@gmail.com>
2024-02-01 02:57:12 +0800

    Rename the startup to startup1 then added a scaffolding for the Identity.

--- Basecode.WebApp/Startup1.cs
+++ Basecode.WebApp/Startup1.cs
@@ -2,9 +2,9 @@
 
 namespace Basecode.WebApp
 {
-    public partial class Startup
+    public partial class Startup1
     {
-        public Startup(IConfiguration configuration)
+        public Startup1(IConfiguration configuration)
         {
             Configuration = configuration;
         }
