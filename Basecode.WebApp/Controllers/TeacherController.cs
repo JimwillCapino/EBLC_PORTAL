3e64d390914236d0c6a39147604d2ca0c50a60bb
JimwillCapino <jimwillcapino@gmail.com>
2024-02-24 11:43:43 +0800

    Vertical Menu

--- Basecode.WebApp/Controllers/TeacherController.cs
+++ Basecode.WebApp/Controllers/TeacherController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity; 
 namespace Basecode.WebApp.Controllers
 {
     [Authorize(Roles = "Teacher")]
     public class TeacherController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        public TeacherController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
         public IActionResult Index()
+        {
+            //var id = _userManager.GetUserId(User);
+            return View();
+        }
+        public IActionResult StudentList()
         {
             return View();
         }
