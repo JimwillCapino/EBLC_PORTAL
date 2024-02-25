f53cedb501014133d386f707914fc250dba98882
JimwillCapino <jimwillcapino@gmail.com>
2024-02-25 17:17:17 +0800

    StudentManagementService: GetStudentBySubject and SubmitGrade function.

---
+++ Basecode.Services/Interfaces/IStudentManagementService.cs
@@ -0,0 +1,16 @@
+﻿using Basecode.Data.Models;
+using Basecode.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Services.Interfaces
+{
+    public interface IStudentManagementService
+    {
+        public List<GradesViewModel> GetStudentGradeBySubject(int student_Id, int subject_Id);
+        public void SubmitGrade(Grades grades);
+    }
+}
--- Basecode.Services/Services/StudentManagementService.cs
+++ Basecode.Services/Services/StudentManagementService.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using Basecode.Data.Interfaces;
+using Basecode.Data.ViewModels;
+using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Basecode.Data;
+using Basecode.Data.Models;
+using Basecode.Services.Interfaces;
 namespace Basecode.Services.Services
 {
-    internal class StudentManagementService
+    public class StudentManagementService:IStudentManagementService
     {
+        private readonly IStudentManagementRepository _studentManagementRepository;
+        public StudentManagementService(IStudentManagementRepository studentManagementRepository) 
+        {
+            _studentManagementRepository = studentManagementRepository;
+        }
+        public List<GradesViewModel> GetStudentGradeBySubject(int student_Id, int subject_Id)
+        {
+            try
+            {
+                return _studentManagementRepository.GetStudentGradeBySubject(student_Id, subject_Id);
+            }
+            catch
+            {
+                throw new Exception(Data.Constants.Exception.DB);
+            }
+        }
+        public void SubmitGrade(Grades grades)
+        {
+            try
+            {
+                _studentManagementRepository.SubmitGrade(grades);
+            }
+            catch
+            {
+                throw new Exception(Data.Constants.Exception.DB);
+            }
+        }
     }
 }
