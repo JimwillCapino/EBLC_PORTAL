5b5729fba6cc51a1ee8b4b068090338df2f90fb9
JimwillCapino <jimwillcapino@gmail.com>
2024-04-10 12:17:06 +0800

    Fix some issues

--- Basecode.Data/ViewModels/TeacherViewModel.cs
+++ Basecode.Data/ViewModels/TeacherViewModel.cs
@@ -13,5 +13,6 @@ namespace Basecode.Data.ViewModels
         public string MiddleName { get; set; }
         public string LastName { get; set; }
         public string Gender { get; set; }
+        public string Email { get; set; }
     }
 }
