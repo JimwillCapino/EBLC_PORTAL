1fcd908a672b96304519be52be413f060a5bff83
JimwillCapino <jimwillcapino@gmail.com>
2024-04-26 01:08:37 +0800

    Modified the SubjectViewModel for validation.

--- Basecode.Data/ViewModels/SubjectViewModel.cs
+++ Basecode.Data/ViewModels/SubjectViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,13 @@ namespace Basecode.Data.ViewModels
     public class SubjectViewModel
     {
         public int subjectid { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         public string subjectname { get; set; }
+        [Required]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Please enter a valid integer.")]
         public int grade { get; set; }
+        [Required]
         public bool haschild { get; set; }
     }
 }
